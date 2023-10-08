41886e49bf12e5029f8ec3d712b6b914e5b3f897
benson-dylan <143048807+benson-dylan@users.noreply.github.com>
2023-10-08 18:44:59 -0400

    All rules for lexer file, and working main file

---
+++ lexer_main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lexer.h"
+#include "utilities.h"
+
+
+int main(int argc, char *argv[])
+{
+    // Error handler already provided
+    if (argv[1] == NULL)
+        bail_with_error("Filename null");
+
+    lexer_init(argv[1]);
+
+    lexer_output();
+    return 0;
+}
